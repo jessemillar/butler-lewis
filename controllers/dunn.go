53fc725b05913e566264fe3863346cba071c4bd4
Jesse Millar <hellojessemillar@gmail.com>
2018-12-10 15:32:36 -0600

    Initial commit

---
+++ controllers/dunn.go
@@ -0,0 +1,52 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jessemillar/dunn/models"
+	"github.com/labstack/echo"
+)
+
+func ArchiveCard(c echo.Context) error {
+	trelloKey := os.Getenv("TRELLO_KEY")
+	trelloToken := os.Getenv("TRELLO_TOKEN")
+	trelloList = "5b1bf0d6b20211b1d693e4a6"
+
+	response, err := http.Get("https://api.trello.com/1/lists/" + trelloList + "/cards?key=" + trelloKey + "&token=" + trelloToken)
+	if err != nil {
+		log.Print(err)
+	} else {
+		defer response.Body.Close()
+
+		// TODO Does reading close the body?
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Print(err)
+		}
+
+		var allCards []models.Card
+		json.Unmarshal(body, &allCards)
+
+		name := c.QueryParam("name")
+
+		// Find the card that matches the passed name
+		for _, c := range allCards {
+			if c.Name == name {
+				// Mark card as done
+				client := &http.Client{}
+				request, err := http.NewRequest("PUT", "https://api.trello.com/1/cards/"+c.ID+"?closed=true&key="+trelloKey+"&token="+trelloToken, nil)
+				_, err = client.Do(request)
+				if err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+
+	// TODO Do I need/want to send a response back?
+	return c.JSON(200, nil)
+}
---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jessemillar/dunn/controllers"
+	"github.com/jessemillar/health"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	log.Println("Configuring server")
+
+	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+
+	e.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
+	e.PUT("/v1/dunn", controllers.ArchiveCard)
+
+	e.Logger.Fatal(e.Start(":9999"))
+}
